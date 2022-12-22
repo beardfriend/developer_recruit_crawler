22cc0ca9bf3c2095fffd1fb9e077c9efae2e5905
beardfriend <beardfriend21@gmail.com>
2022-12-22 12:04:54 +0900

    rollback

--- main.go
+++ main.go
@@ -3,14 +3,11 @@ package main
 import (
 	"html/template"
 	"net/http"
-	"os"
 	"strings"
 
 	"dev_recruitment_crawler/engine"
-	"dev_recruitment_crawler/infrastructure"
 
 	"github.com/gin-gonic/gin"
-	"github.com/jasonlvhit/gocron"
 	"github.com/joho/godotenv"
 )
 
@@ -26,25 +23,12 @@ func main() {
 		panic(err)
 	}
 
-	// mongodb
-	mongo := infrastructure.NewMongodb(os.Getenv("MONGODB_URL"))
-	defer func() {
-		infrastructure.CloseMongodb(mongo)
-	}()
-	e := engine.NewEngine(mongo)
+	e := engine.NewEngine(nil)
 
 	type Request struct {
 		Position string `form:"position,default=backend" binding:"oneof=frontend backend"`
 		Career   int    `form:"career,default=1"`
 	}
-
-	go e.CronRecruitment()
-
-	go func() {
-		s := gocron.NewScheduler()
-		s.Every(1).Hour().Do(e.CronRecruitment)
-		<-s.Start()
-	}()
 
 	g.GET("/", func(ctx *gin.Context) {
 		req := new(Request)
